32bfa082cfcc67b10a2ecf8f54df6476d3af275b
MarekPokornyOva <marek@pokorni-ova.cz>
2026-04-12 15:44:57 +0200

    Fix debug toolbar buttons refresh

--- src/ATuimStudio.Extensions.Debug.Ui/Extension.cs
+++ src/ATuimStudio.Extensions.Debug.Ui/Extension.cs
@@ -38,102 +38,123 @@ namespace ATuimStudio.Extensions.Debug
 			bool CanStop()
 				=> debuggerProvider.Current != null;
 
-			commandRegistrator.Register(StartCommandCode, new AsyncRelayCommand(async (cancellationToken) =>
+			List<IRelayCommand> allCommands = new List<IRelayCommand>(6);
+			T AddCommand<T>(T command) where T : IRelayCommand
 			{
-				async Task<string?> Build(CancellationToken cancellationToken)
+				allCommands.Add(command);
+				return command;
+			}
+
+			void RefreshCommands()
+			{
+				Avalonia.Threading.Dispatcher.UIThread.InvokeAsync(() =>
 				{
-					IServiceProvider sp = commandRegistrator.ServiceProvider;
-					IOutputWriter outputWriter = sp.GetRequiredService<IOutputWriter>();
-					IUiDocumentService uiDocumentService = sp.GetRequiredService<IUiDocumentService>();
+					foreach (IRelayCommand command in allCommands)
+						command.NotifyCanExecuteChanged();
+				});
+			}
 
-					await uiDocumentService.SaveAllOpenedDocuments(cancellationToken);
-
-					IProjectInfo? proj = uiDocumentService.GetActiveDocumentProject();
-					if (proj == null)
+			commandRegistrator.Register(StartCommandCode, AddCommand(new AsyncRelayCommand(async (cancellationToken) =>
+				{
+					async Task<string?> Build(CancellationToken cancellationToken)
 					{
-						outputWriter.Log(OutputType, OutputWriterSeverity.Info, $"No project selected");
-						return null;
+						IServiceProvider sp = commandRegistrator.ServiceProvider;
+						IOutputWriter outputWriter = sp.GetRequiredService<IOutputWriter>();
+						IUiDocumentService uiDocumentService = sp.GetRequiredService<IUiDocumentService>();
+
+						await uiDocumentService.SaveAllOpenedDocuments(cancellationToken);
+
+						IProjectInfo? proj = uiDocumentService.GetActiveDocumentProject();
+						if (proj == null)
+						{
+							outputWriter.Log(OutputType, OutputWriterSeverity.Info, $"No project selected");
+							return null;
+						}
+						IBuildResult result = await sp.GetRequiredService<IBuildService>()
+							.BuildProjectAsync(proj.Name, CancellationToken.None);
+						if (!result.Success)
+							outputWriter.Log(OutputType, OutputWriterSeverity.Error, "Build failed. Try build manually for more detail.");
+						return result.OutputPath;
 					}
-					IBuildResult result = await sp.GetRequiredService<IBuildService>()
-						.BuildProjectAsync(proj.Name, CancellationToken.None);
-					if (!result.Success)
-						outputWriter.Log(OutputType, OutputWriterSeverity.Error, "Build failed. Try build manually for more detail.");
-					return result.OutputPath;
-				}
 
-				string? outputPath = await Build(cancellationToken);
-				if (outputPath == null)
-					return;
+					string? outputPath = await Build(cancellationToken);
+					if (outputPath == null)
+						return;
 
-				IDebugger debugger = await debuggerService.CreateDebuggerAsync(outputPath, null, null, cancellationToken);
+					IDebugger debugger = await debuggerService.CreateDebuggerAsync(outputPath, null, null, cancellationToken);
 
-				void OnStarted(object? sender, BreakpointEventArgs e)
+					void OnStarted(object? sender, BreakpointEventArgs e)
+					{
+						debugger.OnStarted -= OnStarted;
+
+						foreach (Breakpoint bp in breakpointManager.Breakpoints)
+							debugger.ToggleBreakpoint(bp.Filepath, new SourcePosition(bp.Range.Start.Line, bp.Range.Start.Column));
+
+						RefreshCommands();
+					}
+					void DebugTerminated(object? sender, TerminatedEventArgs e)
+					{
+						debuggerIsStopped = true;
+						IDebugger debugger = (IDebugger)sender!;
+						debugger.OnBreakpoint -= DebugBreakpoint;
+						debugger.OnContinued -= DebugContinued;
+						debugger.OnTerminated -= DebugTerminated;
+						RefreshCommands();
+					}
+
+					void DebugBreakpoint(object? sender, BreakpointEventArgs e)
+					{
+						debuggerIsStopped = true;
+						RefreshCommands();
+					}
+
+					void DebugContinued(object? sender, BreakpointEventArgs e)
+					{
+						debuggerIsStopped = false;
+						RefreshCommands();
+					}
+
+					debugger.OnStarted += OnStarted;
+					debugger.OnBreakpoint += DebugBreakpoint;
+					debugger.OnContinued += DebugContinued;
+					debugger.OnTerminated += DebugTerminated;
+					debugger.Start();
+				}, CanStart)),
+				() => AssetLoader.Open(new Uri("avares://ATuimStudio.Extensions.Debug.Ui/Assets/DebugStart.png"))
+			);
+
+			commandRegistrator.Register(ContinueCommandCode, AddCommand(new RelayCommand(() =>
 				{
-					debugger.OnStarted -= OnStarted;
-
-					foreach (Breakpoint bp in breakpointManager.Breakpoints)
-						debugger.ToggleBreakpoint(bp.Filepath, new SourcePosition(bp.Range.Start.Line, bp.Range.Start.Column));
-				}
-				void DebugTerminated(object? sender, TerminatedEventArgs e)
-				{
-					debuggerIsStopped = true;
-					IDebugger debugger = (IDebugger)sender!;
-					debugger.OnBreakpoint -= DebugBreakpoint;
-					debugger.OnContinued -= DebugContinued;
-					debugger.OnTerminated -= DebugTerminated;
-				}
-
-				void DebugBreakpoint(object? sender, BreakpointEventArgs e)
-				{
-					debuggerIsStopped = true;
-				}
-
-				void DebugContinued(object? sender, BreakpointEventArgs e)
-				{
-					debuggerIsStopped = false;
-				}
-
-				debugger.OnStarted += OnStarted;
-				debugger.OnBreakpoint += DebugBreakpoint;
-				debugger.OnContinued += DebugContinued;
-				debugger.OnTerminated += DebugTerminated;
-				debugger.Start();
-			}, CanStart),
+					debuggerProvider.Current?.Continue();
+				}, CanContinue)),
 				() => AssetLoader.Open(new Uri("avares://ATuimStudio.Extensions.Debug.Ui/Assets/DebugStart.png"))
 				);
 
-			commandRegistrator.Register(ContinueCommandCode, new RelayCommand(() =>
-			{
-				debuggerProvider.Current?.Continue();
-			}, CanContinue),
-				() => AssetLoader.Open(new Uri("avares://ATuimStudio.Extensions.Debug.Ui/Assets/DebugStart.png"))
-				);
-
-			commandRegistrator.Register(StopCommandCode, new RelayCommand(() =>
-			{
-				debuggerProvider.Current?.Terminate();
-			}, CanStop),
+			commandRegistrator.Register(StopCommandCode, AddCommand(new RelayCommand(() =>
+				{
+					debuggerProvider.Current?.Terminate();
+				}, CanStop)),
 				() => AssetLoader.Open(new Uri("avares://ATuimStudio.Extensions.Debug.Ui/Assets/DebugStop.png"))
 				);
 
-			commandRegistrator.Register(StepInCommandCode, new RelayCommand(() =>
-			{
-				debuggerProvider.Current?.StepIn();
-			}, CanContinue),
+			commandRegistrator.Register(StepInCommandCode, AddCommand(new RelayCommand(() =>
+				{
+					debuggerProvider.Current?.StepIn();
+				}, CanContinue)),
 				() => AssetLoader.Open(new Uri("avares://ATuimStudio.Extensions.Debug.Ui/Assets/DebugStepIn.png"))
 				);
 
-			commandRegistrator.Register(StepOverCommandCode, new RelayCommand(() =>
-			{
-				debuggerProvider.Current?.StepOver();
-			}, CanContinue),
+			commandRegistrator.Register(StepOverCommandCode, AddCommand(new RelayCommand(() =>
+				{
+					debuggerProvider.Current?.StepOver();
+				}, CanContinue)),
 				() => AssetLoader.Open(new Uri("avares://ATuimStudio.Extensions.Debug.Ui/Assets/DebugStepOver.png"))
 				);
 
-			commandRegistrator.Register(StepOutCommandCode, new RelayCommand(() =>
-			{
-				debuggerProvider.Current?.StepOut();
-			}, CanContinue),
+			commandRegistrator.Register(StepOutCommandCode, AddCommand(new RelayCommand(() =>
+				{
+					debuggerProvider.Current?.StepOut();
+				}, CanContinue)),
 				() => AssetLoader.Open(new Uri("avares://ATuimStudio.Extensions.Debug.Ui/Assets/DebugStepOut.png"))
 				);
 		}
